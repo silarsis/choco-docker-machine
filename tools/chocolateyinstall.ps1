b91d4133a8a0e40a5a1586089f0b6c818053b883
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-02 12:48:49 +0100

    Update docker-machine 0.5.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 $packageName    = 'docker-machine'
-$url            = 'https://github.com/docker/machine/releases/download/v0.5.1/docker-machine_windows-386.zip'
-$checksum       = 'ca6c2e5cc34977b1fb590070557bdbfa'
-$url64          = 'https://github.com/docker/machine/releases/download/v0.5.1/docker-machine_windows-amd64.zip'
-$checksum64     = 'd634acb92fe4b1c73f4b7dec2924291e'
+$url            = 'https://github.com/docker/machine/releases/download/v0.5.2/docker-machine_windows-386.zip'
+$checksum       = '93d5603098d0b4a79ffd91939f8adc02'
+$url64          = 'https://github.com/docker/machine/releases/download/v0.5.2/docker-machine_windows-amd64.zip'
+$checksum64     = '533759e13d66a7d5d536a8a96b5e1227'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
