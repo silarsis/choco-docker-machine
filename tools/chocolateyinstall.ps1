aaa68b1ef0868716887df91f245646c707f37d3a
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-17 12:55:53 +0100

    Remove old docker machine plugins

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -12,5 +12,13 @@ $installDir  = Join-Path "$packageDir" "bin"
 $installBin  = "${packageName}.exe"
 $installPath = Join-Path "$installDir" "$installBin"
 
+if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\docker-machine")) {
+  if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\docker-machine\tools")) {
+    # clean old plugins and ignore files
+    Write-Host "Removing old docker-machine plugins"
+    Remove-Item "$env:ChocolateyInstall\lib\docker-machine\tools\docker-machine-*.*"
+  }
+}
+
 New-Item -ItemType Directory -Force -Path "$installDir"
 Get-ChocolateyWebFile "$packageName" "$installPath" "$url" "$url64" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
