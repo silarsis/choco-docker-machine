43bf472b1a8482b1734e87dea51e68efc96a8abe
Stefan Scherer <scherer_stefan@icloud.com>
2015-12-17 09:08:06 +0100

    Update to docker-machine 0.5.4

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,16 @@
 $packageName    = 'docker-machine'
-$url            = 'https://github.com/docker/machine/releases/download/v0.5.2/docker-machine_windows-386.zip'
-$checksum       = '93d5603098d0b4a79ffd91939f8adc02'
-$url64          = 'https://github.com/docker/machine/releases/download/v0.5.2/docker-machine_windows-amd64.zip'
-$checksum64     = '533759e13d66a7d5d536a8a96b5e1227'
+$url            = 'https://github.com/docker/machine/releases/download/v0.5.4/docker-machine_windows-386.exe'
+$checksum       = 'ab53930550dc9cc269106651af1ead63'
+$url64          = 'https://github.com/docker/machine/releases/download/v0.5.4/docker-machine_windows-amd64.exe'
+$checksum64     = '7735afc2f5f431d9b685b74fc9884c59'
 $checksumType   = 'md5'
 $checksumType64 = 'md5'
-$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage "docker-machine" "$url" "$unzipLocation" "$url64" `
- -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageDir  = "$(Split-Path -parent $toolsDir)"
+$installDir  = Join-Path "$packageDir" "bin"
+$installBin  = "${packageName}.exe"
+$installPath = Join-Path "$installDir" "$installBin"
+
+New-Item -ItemType Directory -Force -Path "$installDir"
+Get-ChocolateyWebFile "$packageName" "$installPath" "$url" "$url64" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
