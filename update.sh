e2e37bf775b5d691993ec6b506f65cd3b0425498
Stefan Scherer <scherer_stefan@icloud.com>
2016-04-15 14:22:05 +0200

    Fix download link
    Signed-off-by: Stefan Scherer <scherer_stefan@icloud.com>

--- update.sh
+++ update.sh
@@ -13,8 +13,8 @@ if [[ "${OSTYPE}" != "darwin"* ]]; then
 fi
 
 version=$1
-url="https://github.com/docker/machine/releases/download/${version}/docker-machine-Windows-i386.exe"
-url64="https://github.com/docker/machine/releases/download/${version}/docker-machine-Windows-x86_64.exe"
+url="https://github.com/docker/machine/releases/download/v${version}/docker-machine-Windows-i386.exe"
+url64="https://github.com/docker/machine/releases/download/v${version}/docker-machine-Windows-x86_64.exe"
 
 checksum=$(curl -L "${url}" | md5) #  | cut -f 1 -d " ")
 checksum64=$(curl -L "${url64}" | md5) #  | cut -f 1 -d " ")
